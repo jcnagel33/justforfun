f653ca39479aff542a7fb2fe74d47127ad81e2fe
jcnagel33 <68299682+jcnagel33@users.noreply.github.com>
2021-06-03 08:52:02 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,5 @@
+music.play_melody("C5 G B A C C5 D F ", 120)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,4 @@
+music.playMelody("C5 G B A C C5 D F ", 120)
+basic.forever(function on_forever() {
+    
+})
